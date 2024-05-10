8e0e870115cbd5ec6b373a308763febaf121e518
aasiskrk <aasis.krk1@gmail.com>
2024-05-10 09:56:01 +0545

    bh

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 // Importing Express
-const express = require('express');
-const dotenv = require('dotenv');
-const mongoose = require('mongoose');
-const connectDB = require('./database/database');
-
+const express = require("express");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const connectDB = require("./database/database");
+const cors = require("cors");
 
 // Crearing an express app
 const app = express();
 
 //JSON Config
 app.use(express.json());
+
+//Cors config
+const corsOptions = {
+  origin: true,
+  credentials: true,
+  optionSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
 
 //Configuration dotenv
 dotenv.config();
@@ -21,32 +30,31 @@ connectDB();
 const PORT = process.env.PORT;
 
 // Creating a Test Route or endpoint
-app.get("/test", (req,res)=>{
-    res.send('test api is working ... !')
-})
+app.get("/test", (req, res) => {
+  res.send("test api is working ... !");
+});
+
+app.get("/test_new", (req, res) => {
+  res.send("test new api is working ... !");
+});
 
 //configuring user Routes
-app.use('/api/user', require('./routes/userRoutes'))
+app.use("/api/user", require("./routes/userRoutes"));
 
-app.use('/api/product', require('./routes/productRoutes'))
-
-
+app.use("/api/product", require("./routes/productRoutes"));
 
 // API URL
 // http://localhost:5000/api/user/create
 
-
-
 // Starting the server
-app.listen(PORT, ()=> {
-    console.log(`Server  is running on  port: ${PORT} 
+app.listen(PORT, () => {
+  console.log(`Server  is running on  port: ${PORT} 
 // API URL
-// http://localhost:5000/api/user/create`)
-})
-
+// http://localhost:5000/api/user/create`);
+});
 
 //Task 1 Controller - Routes - Index.js
-//Make a productController .js 
+//Make a productController .js
 //Make a productRoutes.js
 //link to index.js
 //http://localhost:5000/api/product/create
