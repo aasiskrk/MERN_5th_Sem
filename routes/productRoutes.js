3d65f9343080a43faf218742a2cbfd558dcea956
aasiskrk <aasiskrk1@gmail.com>
2024-06-11 07:04:11 +0545

    c

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -2,16 +2,18 @@
 
 const router = require("express").Router();
 const productContoller = require("../controllers/productController");
+const { authGuard } = require("../middleware/authGuard");
 
 //Make a create user API
 
 router.post("/create", productContoller.createProduct);
 
 //fetch all products
-router.get("/get_all_products", productContoller.getAllProducts);
+//http://localhost:5000/api/product/get_all_products
+router.get("/get_all_products", authGuard, productContoller.getAllProducts);
 
 //fetch single product
-router.get("/get_single_product/:id", productContoller.getProduct);
+router.get("/get_single_product/:id",authGuard, productContoller.getProduct);
 
 //delete product
 router.delete("/delete_product/:id", productContoller.deleteProduct);
